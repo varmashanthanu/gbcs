86a83f787f2d717b776244beb76190afc3033d25
staanz <staanz@gmail.com>
2017-06-03 15:19:49 -0400

    Added models Skills, Addresses, Teams, Competitions. Next Lot: Relationship models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+
+  get 'welcome/contact_us'
+
+  get 'welcome/about_us'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root to: 'welcome#home'
 end
