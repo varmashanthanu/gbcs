37db777074619d058d6b0116a674d611f677e4ea
staanz <staanz@gmail.com>
2017-06-04 15:52:35 -0400

    Added Sidebar. Added Devise views and controllers. Fixed some formatting for Nav/Side/Content/Footer. Now proceeding with User model.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # For Devise
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
   get 'welcome/home'
 
   get 'welcome/contact_us'
 
   get 'welcome/about_us'
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  devise_for :users#, controllers: {confirmations: 'users/confirmations', passwords: 'users/passwords', registrations: 'users/registrations', sessions: 'users/sessions', unlocks: 'users/unlocks'}
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 
   root to: 'welcome#home'
 end
