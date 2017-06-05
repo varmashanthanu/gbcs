cc6a1e1323b35f80771c66e126813f7572299b74
staanz <staanz@gmail.com>
2017-06-05 13:09:42 -0400

    Added Password update to users.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
 
   get 'welcome/about_us'
 
-
+  # get 'users/edit_password' => 'users#edit_password'
+  match 'users/edit_password' => 'users#edit_password', as: :user_edit_password, via: [:get]
+  match 'users/update_password' => 'users#update_password', as: :user_update_password, via: [:post]
 
   devise_for :users#, controllers: {confirmations: 'users/confirmations', passwords: 'users/passwords', registrations: 'users/registrations', sessions: 'users/sessions', unlocks: 'users/unlocks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
