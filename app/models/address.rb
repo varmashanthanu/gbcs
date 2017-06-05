dfffa45a6f90d92504530cc6185977fce24db8c3
staanz <staanz@gmail.com>
2017-06-05 02:50:02 -0400

    Avatar Uploader added, fixed. User edit/update page & controller#action fixed. Avatar defaults fixed. Image edited/added. Sidebar formatting / centering done. User model name / fullname methods deffed.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+
+  belongs_to :addressable, :polymorphic => true, optional: true
+
+  geocoded_by :addr
+  after_validation :geocode, :if => :addr.present? and :addr_changed?
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable
+
+  attr_accessor :address_attributes
+
+  has_one :address, as: :addressable, dependent: :destroy
+  accepts_nested_attributes_for :address, reject_if: proc { |attributes| attributes[:addr].blank? }, allow_destroy: true
+
+  has_many :user_skills, dependent: :destroy
+  has_many :skills, through: :user_skills
+
+  has_many :members, dependent: :destroy
+  has_many :teams, through: :members
+
+  mount_uploader :avatar, AvatarUploader
+
+  # Name Calls
+  def name
+    (fname && fname!='' || lname && lname!='')? (fname||lname):email.split('@')[0].humanize
+  end
+
+  def fullname
+    (fname && fname!='' || lname && lname!='')? "#{fname} #{lname}":name
+  end
+
 end
+
