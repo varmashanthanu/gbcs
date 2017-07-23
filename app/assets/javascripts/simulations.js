14d2dee79e9641a2b32142e4df592fa27182ad5a
staanz <staanz@gmail.com>
2017-07-24 00:10:52 +0530

    Major: Added Pagination to Simulations with AJAX. Minor: May have made a bar chart action in simulations controller obsolete.

---
+++ app/assets/javascripts/simulations.js
@@ -0,0 +1,10 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.erb.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(function(){
+    $('.pagination a').on('click',function(){
+        $.get(this.href,null,null,'script');
+        return false;
+    });
+});
--- app/controllers/simulations_controller.rb
+++ app/controllers/simulations_controller.rb
@@ -4,17 +4,13 @@ class SimulationsController < ApplicationController
   def index
     @team = Team.find(params[:team])
     params[:members] ? @members = User.where(:id => params[:members]) : @members = User.where(:id => @team.members.pluck(:user_id))
-    @users = search_params
+    @users = search_params.paginate(page: params[:page], per_page: 15)
   end
 
   def search
     @team = Team.find(params[:team][:id])
     params[:members].present? ? @members = User.find((params[:members][:id])) : @members = User.where(:id => @team.members.pluck(:user_id))
-    Rails.logger.debug("Testing #{params[:members][:id]}") #TODO Remove
-    Rails.logger.debug("Testing #{params[:members][:id].class}") #TODO Remove
-    Rails.logger.debug("Testing #{User.where(id:params[:members][:id]).count}") #TODO Remove
-    Rails.logger.debug("Testing #{@members.pluck(:id)}") #TODO Remove
-    @users = search_params
+    @users = search_params.paginate(page: params[:page], per_page: 15)
     respond_to do |format|
       format.html
       format.js
@@ -26,9 +22,8 @@ class SimulationsController < ApplicationController
     params[:members].present? ? params[:members] << params[:new_member] : params[:members] = params[:new_member]
     @team = Team.find(params[:team])
     @members = User.where(:id => params[:members])
-    Rails.logger.debug("Action NEW #{@members.count} #{@members.first.name}")
     @compatibility = YesList.compatibility(@members)
-    @users = search_params
+    @users = search_params.paginate(page: params[:page], per_page: 15)
     respond_to do |format|
       format.html
       format.js
@@ -40,10 +35,8 @@ class SimulationsController < ApplicationController
     @member = params[:remove_member].to_i
     @team = Team.find(params[:team])
     params[:members] -= [params[:remove_member]]
-    Rails.logger.debug("TESTONG #{params[:members]}") # TODO REMOVE
-    Rails.logger.debug("TESTONG #{@member.class}, member-#{@member}") # TODO REMOVE
     @members = User.find(params[:members])
-    @users = search_params
+    @users = search_params.paginate(page: params[:page], per_page: 15)
     respond_to do |format|
       format.html
       format.js
@@ -60,7 +53,6 @@ class SimulationsController < ApplicationController
 
   def team_skills
     @members = User.find(params[:members])
-    Rails.logger.debug("TESTINGTESTING #{User.combined_skills(@members)}")
     render json: User.combined_skills(@members)
   end
 
@@ -72,14 +64,10 @@ class SimulationsController < ApplicationController
 
   def search_params #TODO remove loggers
     user = User.active.where.not(:id=>@members.pluck(:id))
-    (user = user.where("fname iLIKE ? OR lname iLIKE ?", "%#{params[:name]}%", "%#{params[:name]}%");Rails.logger.debug("Triggered 3")) if (params[:name] && params[:name] != '')
+    (user = user.where("fname iLIKE ? OR lname iLIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")) if (params[:name] && params[:name] != '')
 
-    params.each do |k,v|
-      Rails.logger.debug("Key #{k}, Value #{v}")
-    end
     if params[:sp].present? && (params[:sp][:sp1].present? || params[:sp][:sp2].present? || params[:sp][:sp3].present?)
         user = user.search(params[:sp],params[:so])
-        Rails.logger.debug("Triggered 1 #{user.class}")
     else
       current_user.admin ? user = user.students.order(:program).order(score:'DESC') : user = user.gen_sort(current_user)
     end
