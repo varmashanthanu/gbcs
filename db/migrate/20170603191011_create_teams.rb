3f1f60289a27b57a2e6c2709f6784f30246607bf
staanz <staanz@gmail.com>
2017-09-18 21:03:08 +0530

    some retarded issue with teams table

--- db/migrate/20170603191011_create_teams.rb
+++ db/migrate/20170603191011_create_teams.rb
@@ -1,12 +1,12 @@
 class CreateTeams < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
+    create_table :teams do |t|
       t.string :name
       t.string :avatar
       t.integer :lead_id
 
       t.timestamps
     end
-    add_index :users, :name
+    add_index :teams, :name
   end
 end
